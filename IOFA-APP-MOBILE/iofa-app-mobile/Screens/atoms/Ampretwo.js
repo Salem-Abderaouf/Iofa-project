2322cf5e6553ed0b371046b1f7876e7241aa13c7
Salem-Abderaouf <salamasalem84@gmail.com>
2022-01-06 17:21:48 +0100

    create and update two Ampre chart

---
+++ IOFA-APP-MOBILE/iofa-app-mobile/Screens/atoms/Ampretwo.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Dimensions } from 'react-native'
+import RNSpeedometer from 'react-native-speedometer'
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+
+const Ampretwo = (props) => {
+  const [ampValue2, setAmpValue2] = useState(0);
+
+  useEffect(() => {
+    const userID = auth().currentUser;
+    const dbRef = database().ref(`users/${userID.uid}/motor/`)
+    dbRef
+      .on('value', (res) => {
+        if (res.val() !== null && props.startPumb) {
+          setAmpValue2(res.val().amp2)
+        }
+        else {
+          setAmpValue2(0);
+        }
+      })
+  }, [ampValue2, props.startPumb])
+
+  const labels = [
+    {
+      name: 'low',
+      labelColor: '#f4ab44',
+      activeBarColor: '#f4ab44',
+
+    },
+    {
+      name: 'Medium',
+      labelColor: '#ff5400',
+      activeBarColor: '#ff5400',
+    },
+    {
+      name: 'High',
+      labelColor: '#ff2900',
+      activeBarColor: '#ff2900',
+    }
+  ]
+
+  return (
+    <View style={styles.SpeedBox}>
+
+      <RNSpeedometer
+        labels={labels}
+        value={ampValue2}
+        size={100}
+        minValue={0}
+        maxValue={100}
+      />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  wrapper: {
+    marginVertical: 50,
+    width: Dimensions.get('window').width - 30,
+    alignSelf: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+  },
+  SpeedBox: {
+    width: Dimensions.get('window').width / 2
+  }
+})
+export default Ampretwo;
